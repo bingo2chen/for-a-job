75acb31080e9626262022cff0e7de4363b8a7051
ghosThot <1018513521@qq.com>
2020-10-23 21:24:31 +0800

    promise 简版

---
+++ 2.promise.js
@@ -0,0 +1,51 @@
+const PENDING = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
+
+function MyPromise(fn) {
+  const _this = this
+  _this.state = PENDING
+  _this.value = null
+  _this.resolvedCallbacks = []
+  _this.rejectedCallbacks = []
+
+  function resolved(value) {
+    if (_this.state === PENDING) {
+      _this.state = RESOLVED
+      _this.value = value
+      _this.resolvedCallbacks.map(cb => cb(_this.value))
+    }
+  }
+
+  function rejected(value) {
+    if (_this.state === PENDING) {
+      _this.state = REJECTED
+      _this.value = value
+      _this.rejectedCallbacks.map(cb => cb(_this.value))
+    }
+  }
+}
+
+MyPromise.prototype.then = function (onFullFilled, onRejected) {
+  const _this = this
+  onFullFilled = 
+    typeof onFullFilled === 'function' 
+      ? onFullFilled 
+      : v => v
+  onRejected = 
+    typeof onRejected === 'function' 
+      ? onRejected
+      : r => {
+        throw r
+      }
+  if (_this.state === PENDING) {
+    _this.resolvedCallbacks.push(onFullFilled)
+    _this.rejectedCallbacks.push(onRejected)
+  }
+  if (_this.state === RESOLVED) {
+    onFullFilled(_this.value)
+  }
+  if (_this.state === REJECTED) {
+    onRejected(_this.value)
+  }
+}
